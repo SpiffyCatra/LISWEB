6501024e9c0a1b2954c6bf6c75dbc9a3431b57c2
SpiffyCatra <Symsym4244@gmail.com>
2023-04-14 19:34:43 -0400

    Looking good, color

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,16 @@
  <div>
 
  <h1 class="text-center"> Life Is So Strange </h1>
-<p> Life is strange is a decision making game designed for only the most gayest queers imaginable. *cough cough* Me, clearly. Since it came out in 
+<p class="purple--text"> Life is strange is a decision making game designed for only the most gayest queers imaginable. *cough cough* Me, clearly. Since it came out in 
 January 30th, 2015 it has been loved by many tumblr, amino and instagram users. Life is Strange follows a young college student by the name of
 Max Caulfield as she finds that her gay ass self has some pretty gay ass power to rewind time.</p>
 <h2> Max Caulfield</h2> 
+<v-card> 
+<v-card-text> 
+Max Caulfield is a young adult who studies and does photography.
+
+</v-card-text>
+</v-card>
 
  </div>
  
