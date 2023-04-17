ab3972d7233287e6866def78f6ebee4fb02f49ce
SpiffyCatra <Symsym4244@gmail.com>
2023-04-17 11:51:05 -0400

    WIMC test

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,14 @@
  <div>
 
  <h1 class="text-center"> Life Is So Strange </h1>
-<p class="purple--text"> Life is strange is a decision making game designed for only the most gayest queers imaginable. *cough cough* Me, clearly. Since it came out in 
-January 30th, 2015 it has been loved by many tumblr, amino and instagram users. Life is Strange follows a young college student by the name of
-Max Caulfield as she finds that her gay ass self has some pretty gay ass power to rewind time.</p>
+<p class="purple--text"> Life is strange is a decision making game that asks the player a specific question. "If you could turn
+back the clock, would you do it?" The game gives you the power to go back into time, often allowing you to retract a decision or even
+undo an action. You play as Max Caulfield as every decision you make or do not make effect the people around you. The game makes it
+that in most cases there is no right or wrong decision but instead, a cause and effect set of instances. 
+for only the most gayest queers imaginable. *cough cough* Me, clearly. Since it came out on 
+January 30th, 2015 it has become a little dated, still holding a charm of slang and aesthetic used in that year.</p>
+
+
 <h2> Max Caulfield</h2> 
 <v-card> 
 <v-card-text> 
@@ -12,7 +17,8 @@ Max Caulfield is a young adult who studies and does photography.
 
 </v-card-text>
 </v-card>
-
+ 
+ <h3> But who is Max Caulfeild?</h3>
  </div>
  
 </template>
