c0fd5c841af4c206de19a9bba65794bdc3c1223d
SpiffyCatra <Symsym4244@gmail.com>
2023-04-17 12:09:40 -0400

    V-card test

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,40 @@
 <template>
  <div>
 
+ 
+
  <h1 class="text-center"> Life Is So Strange </h1>
 <p class="purple--text"> Life is strange is a decision making game that asks the player a specific question. "If you could turn
 back the clock, would you do it?" The game gives you the power to go back into time, often allowing you to retract a decision or even
 undo an action. You play as Max Caulfield as every decision you make or do not make effect the people around you. The game makes it
 that in most cases there is no right or wrong decision but instead, a cause and effect set of instances. 
-for only the most gayest queers imaginable. *cough cough* Me, clearly. Since it came out on 
 January 30th, 2015 it has become a little dated, still holding a charm of slang and aesthetic used in that year.</p>
 
 
 <h2> Max Caulfield</h2> 
 <v-card> 
 <v-card-text> 
-Max Caulfield is a young adult who studies and does photography.
+Max Caulfield is a young adult who studies and does photography at Blackwell academy. 
 
 </v-card-text>
 </v-card>
  
+
  <h3> But who is Max Caulfeild?</h3>
+ <v-card>
+ <v-card-text>
+ 
+ Max is the main character who is controlled by the player and because of this a lot of people do not seem to think that Max still has her
+own personality. For the most part, she is not a party gal and she does have a bit of the "not like other girls" personality which could br
+credited to the time it was made. Regardless, the decisions you can make with her in the game can change how she appears. For instance,
+the first big decision required is whether or not you tell on Nathan having a gun to the principal, it would be considered bold to tell him
+about the gun because of Nathans status.
+ 
+ </v-card-text>
+ 
+ </v-card>
+
+
  </div>
  
 </template>
